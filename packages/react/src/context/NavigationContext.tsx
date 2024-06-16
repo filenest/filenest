3611cabc660db3a003b2131380adb67e071fd98b
nordowl <jonas@nordstudio.digital>
2024-06-16 02:46:23 +0200

    chore(react): remove unused import

--- packages/react/src/context/NavigationContext.tsx
+++ packages/react/src/context/NavigationContext.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext } from "react"
 import type {  Folder } from "@filenest/handlers"
 import { useGlobalContext } from "./GlobalContext"
 
