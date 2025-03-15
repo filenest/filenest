b78a9c777b216a2f17d36a1881e3cbe52dd3e09b
nordowl <jonas@nordstudio.digital>
2025-03-15 02:46:51 +0100

    refactor: adapter clients

--- packages/core/src/adapter.ts
+++ packages/core/src/adapter.ts
@@ -2,7 +2,7 @@ import { Provider } from "."
 import { getHandlersFromProvider } from "./utils"
 import { ClientAPICallerREST, ClientAPICallerTRPC } from "./client"
 
-export interface AdapterClientConfig {
+export interface AdapterClient {
     fetchers: ReturnType<typeof getHandlersFromProvider>
 }
 
@@ -11,108 +11,67 @@ interface AdapterClientOptions {
     onError?: (message: string) => void
 }
 
-export type MakeAdapterClient = (options: AdapterClientOptions) => AdapterClientConfig
+export type MakeAdapterClient = (options: AdapterClientOptions) => AdapterClient
 
-export const RESTClient: MakeAdapterClient = (options) => {
-    const { endpoint, onError } = options
+const prepareClient = (
+    APICaller: typeof ClientAPICallerREST | typeof ClientAPICallerTRPC,
+    s: string
+): MakeAdapterClient => {
+    return (options: AdapterClientOptions) => {
+        const { endpoint, onError } = options
 
-    const caller = new ClientAPICallerREST(endpoint, onError)
+        const caller = new APICaller(endpoint, onError)
 
-    return {
-        fetchers: {
-            files: {
-                async getFiles(input) {
-                    return await caller.call("/files/getFiles", input)
-                },
-                async deleteFiles(input) {
-                    return await caller.call("/files/deleteFiles", input, {
-                        method: "POST",
-                    })
-                },
-                async getUploadUrl(input) {
-                    return await caller.call("/files/getUploadUrl", input)
-                },
-                async getRequiredParams() {
-                    return await caller.call("/files/getRequiredParams", undefined)
-                },
-                async updateFile(input) {
-                    return await caller.call("/files/updateFile", input, {
-                        method: "POST",
-                    })
-                },
-            } satisfies Provider["files"],
-            folders: {
-                async getFolders(input) {
-                    return await caller.call("/folders/getFolders", input)
-                },
-                async createFolder(input) {
-                    return await caller.call("/folders/createFolder", input, {
-                        method: "POST",
-                    })
-                },
-                async deleteFolder(input) {
-                    return await caller.call("/folders/deleteFolder", input, {
-                        method: "POST",
-                    })
-                },
-                async updateFolder(input) {
-                    return await caller.call("/folders/updateFolder", input, {
-                        method: "POST",
-                    })
-                },
-            } satisfies Provider["folders"],
-        },
+        return {
+            fetchers: {
+                files: {
+                    async getFiles(input) {
+                        return await caller.call(`${s}files${s}getFiles`, input)
+                    },
+                    async deleteFiles(input) {
+                        return await caller.call(`${s}files${s}deleteFiles`, input, {
+                            method: "POST",
+                        })
+                    },
+                    async getUploadUrl(input) {
+                        return await caller.call(`${s}files${s}getUploadUrl`, input)
+                    },
+                    async getRequiredParams() {
+                        return await caller.call(
+                            `${s}files${s}getRequiredParams`,
+                            undefined
+                        )
+                    },
+                    async updateFile(input) {
+                        return await caller.call(`${s}files${s}updateFile`, input, {
+                            method: "POST",
+                        })
+                    },
+                } satisfies Provider["files"],
+                folders: {
+                    async getFolders(input) {
+                        return await caller.call(`${s}folders${s}getFolders`, input)
+                    },
+                    async createFolder(input) {
+                        return await caller.call(`${s}folders${s}createFolder`, input, {
+                            method: "POST",
+                        })
+                    },
+                    async deleteFolder(input) {
+                        return await caller.call(`${s}folders${s}deleteFolder`, input, {
+                            method: "POST",
+                        })
+                    },
+                    async updateFolder(input) {
+                        return await caller.call(`${s}folders${s}updateFolder`, input, {
+                            method: "POST",
+                        })
+                    },
+                } satisfies Provider["folders"],
+            },
+        }
     }
 }
 
-export const TRPCClient: MakeAdapterClient = (options) => {
-    const { endpoint, onError } = options
-
-    const caller = new ClientAPICallerTRPC(endpoint, onError)
-
-    return {
-        fetchers: {
-            files: {
-                async getFiles(input) {
-                    return await caller.call(".files.getFiles", input)
-                },
-                async deleteFiles(input) {
-                    return await caller.call(".files.deleteFiles", input, {
-                        method: "POST",
-                    })
-                },
-                async getUploadUrl(input) {
-                    return await caller.call(".files.getUploadUrl", input)
-                },
-                async getRequiredParams() {
-                    return await caller.call(".files.getRequiredParams", undefined)
-                },
-                async updateFile(input) {
-                    return await caller.call(".files.updateFile", input, {
-                        method: "POST",
-                    })
-                },
-            } satisfies Provider["files"],
-            folders: {
-                async getFolders(input) {
-                    return await caller.call(".folders.getFolders", input)
-                },
-                async createFolder(input) {
-                    return await caller.call(".folders.createFolder", input, {
-                        method: "POST",
-                    })
-                },
-                async deleteFolder(input) {
-                    return await caller.call(".folders.deleteFolder", input, {
-                        method: "POST",
-                    })
-                },
-                async updateFolder(input) {
-                    return await caller.call(".folders.updateFolder", input, {
-                        method: "POST",
-                    })
-                },
-            } satisfies Provider["folders"],
-        },
-    }
-}
+export const RESTClient = prepareClient(ClientAPICallerREST, "/")
+export const TRPCClient = prepareClient(ClientAPICallerTRPC, ".")
