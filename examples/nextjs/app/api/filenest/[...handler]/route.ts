95d08b3e3c416a75e5e46931a46b83c99323815a
nordowl <jonas@nordstudio.digital>
2025-04-12 01:26:04 +0200

    feat: wip docs

--- examples/nextjs/app/api/filenest/[...handler]/route.ts
+++ examples/nextjs/app/api/filenest/[...handler]/route.ts
@@ -2,7 +2,7 @@ import { initNextjsAdapter } from "@filenest/adapter-nextjs"
 import { UploadThing } from "@filenest/provider-uploadthing"
 
 const provider = new UploadThing({
-    UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN!,
+  UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN!,
 })
 
 export const { GET, POST } = initNextjsAdapter(provider).create()
@@ -11,11 +11,11 @@ export const { GET, POST } = initNextjsAdapter(provider).create()
 Example of using middleware:
 
 export const { GET, POST } = initNextjsAdapter(provider)
-    .use(async (req) => {
-        const isAuthed = req.cookies.get("authed")
-        if (!isAuthed?.value) {
-            return NextResponse.json("Unauthorized", { status: 401 })
-        }
-    })
-    .create()
+  .use(async (req) => {
+    const isAuthed = req.cookies.get("authed")
+    if (!isAuthed?.value) {
+      return NextResponse.json("Unauthorized", { status: 401 })
+    }
+  })
+  .create()
 */
